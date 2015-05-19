ad4067f34a1d83ecb9e7373e9c0478e52d79722c
VijayDwibhashi <VijayDwibhashi@users.noreply.github.com>
2015-05-19 23:43:08 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse.
+## Part of the pair of functions to cache the inverse of a matrix
 
-## Write a short comment describing this function
+## Uses the name of makeCacheMatrix as required
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mtx = matrix()) {
 
+        ## Initialize the INVERSE Matrix 
+        inverse <- NULL
+        
+        ## SET the Matrix 
+        set <- function(x) {
+                mtx <<- x;
+                inverse <<- NULL;
+        }
+
+        ## RETURN the matrix value 
+        get <- function() return(mtx);
+
+        ## SETING the inverse matrix
+        setinv <- function(inv) inverse <<- inv;
+
+        ## RETURN the inverse matrix 
+        getinv <- function() return(inverse);
+
+        return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+## above. If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Geting the value of Inverse value of the matrix
+        inv <- x$getinv()
+        
+        ## Return the value of inverse if it is already cached 
+	 if(!is.null(inv)) {
+                message("Inverse Cached")
+                return(inv)
+        }
+        
+        ## If the inverse is not already calculated, get the matrix and caluclate the inverse
+        data <- x$get()
+        m <- solve(data) ## Calculate Inverse
+        x$setinv(m) ## Cache the value of Inverse
+        m ## retunn the inverse matrix 
 }
